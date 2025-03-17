cdb79cb14650e720cf6eb461e0ca6f134def9d40
Artemius <artemcka.jdanov@yandex.ru>
2025-03-17 21:21:04 +0700

    update GameController.tsx, added timer

--- red-light-green-light/src/components/GameController.tsx
+++ red-light-green-light/src/components/GameController.tsx
@@ -5,13 +5,35 @@ import Player from './Player';
 const GameController: React.FC = () => {
   const [isGreen, setIsGreen] = useState(true);
   const [isGameOver, setIsGameOver] = useState(false);
+  const [isBlink, setIsBlink] = useState(false);
+  const [timeLeft,setTimeLeft] = useState(30);
+
+
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setIsGreen((prev) => !prev);
-    }, 3000); 
-    return () => clearInterval(interval);
+    const lightInterval = setInterval(() => {
+      setIsBlink(true); 
+      setTimeout(() => {
+        setIsGreen((prev) => !prev);
+        setIsBlink(false);
+      }, 2000); 
+    }, 5000); 
+
+    useEffect(() => {
+      if (timeLeft > 0 && !isGameOver) {
+        const timer = setInterval(() => {
+          setTimeLeft((prev) => prev - 1);
+        }, 1000);
+        return () => clearInterval(timer);
+      } else if (timeLeft === 0) {
+        setIsGameOver(true); 
+      }
+    }, [timeLeft, isGameOver]);
+
+    
+    return () => clearInterval(lightInterval);
   }, []);
+
 
   const handleCaught = () => {
     setIsGameOver(true);
@@ -24,7 +46,7 @@ const GameController: React.FC = () => {
         <h2>Game over</h2>
       ) : (
         <>
-          <Light isGreen={isGreen} />
+          <Light isGreen={isGreen} isBlink ={isBlink} />
           <Player isMove={isGreen} isCaught={handleCaught} />
         </>
       )}
